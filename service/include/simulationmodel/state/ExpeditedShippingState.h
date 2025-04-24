8e06e1e4842b665f9eecbe21367a6dbae73b4379
Xander <hill1594@umn.edu>
2025-04-24 14:18:44 -0500

    Expedited Shipping State

---
+++ service/include/simulationmodel/state/ExpeditedShippingState.h
@@ -0,0 +1,22 @@
+#ifndef EXPEDITED_SHIPPING_STATE_H_
+#define EXPEDITED_SHIPPING_STATE_H_
+
+#include "PriorityShippingState.h"
+#include "Package.h"
+
+/**
+ * @brief Expedited Shipping State
+*/
+class ExpeditedShippingState : public PriorityShippingState {
+    private:
+        *Package package;
+
+    public:
+        /**
+         * @brief Gets Shipping Prioirty of current package
+         * @return int representing highest shipping priority
+        */
+       int getPriority() { return 0; }
+};
+
+#endif
