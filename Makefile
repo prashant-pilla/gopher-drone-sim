f9a674206ef8a27641f8775ab97a9b6e35c2cc4a
Xander <hill1594@umn.edu>
2025-05-05 16:41:28 -0500

    queue header file

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # This Makefile provides you with quick access to usefule commands
 
-PORT = 8081
+PORT = 8086
 BUILD_DIR = build
 TRANSITE_EXE = $(BUILD_DIR)/bin/transit_service
 
---
+++ service/include/simulationmodel/state/ShippingQueue.h
@@ -0,0 +1,47 @@
+#ifndef SHIPPING_QUEUE_H_
+#define SHIPPING_QUEUE_H_
+
+#include "Package.h"
+#include <vector>
+
+class Package;
+
+/**
+ * @brief class for Shipping queue structure
+*/
+class ShippingQueue {
+    public:
+        /**
+         * @brief constructor
+        */
+       ShippingQueue();
+
+       /**
+        * @brief add a package to the queue
+        * @param pkg package to be added to queue
+       */
+      void addPackage(Package& pkg);
+
+      /**
+       * @brief remove a package from the queue
+       * @param pkg package to be removed
+      */
+     void removePackage(Package& pkg);
+
+     /**
+      * @brief update the priority of a package
+      * @param pkg package to be updated
+      * @param level new priority shipping level
+     */
+        void updatePackage(Package& pkg, int level);
+
+        /**
+         * @brief sort queue by priority
+        */
+    void sortQueue();
+
+    private:
+        std::vector<Package> packages;
+};
+
+#endif
