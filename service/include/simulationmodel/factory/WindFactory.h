80ceb8e51838c288c8d5fd2f2038f1705f2a694a
Casey Paulson <paul1401@umn.edu>
2025-05-09 11:21:19 -0500

    other half of severe weather

---
+++ service/include/simulationmodel/factory/WindFactory.h
@@ -0,0 +1,21 @@
+#ifndef WIND_FACTORY_H_
+#define WIND_FACTORY_H_
+
+#include "WeatherControl.h"
+#include "IEntityFactory.h"
+
+/**
+ *@brief Wind Factory to produce WeatherControl class.
+ **/
+class WindFactory : public IEntityFactory {
+ public:
+  /**
+   * @brief Creates entity using the given JSON object, if possible.
+   * @param entity - JsonObject to be used to create the new entity.
+   * @return Entity that was created if it was created successfully, or a
+   *nullpointer if creation failed.
+   **/
+  IEntity* createEntity(const JsonObject& entity);
+};
+
+#endif
