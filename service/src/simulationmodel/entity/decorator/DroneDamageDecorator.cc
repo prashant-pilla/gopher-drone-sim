c09db75f9b2c5f8069f36e0fb3ed896266ce2697
Casey Paulson <paul1401@umn.edu>
2025-05-09 12:45:31 -0500

    linted

--- service/src/simulationmodel/entity/decorator/DroneDamageDecorator.cc
+++ service/src/simulationmodel/entity/decorator/DroneDamageDecorator.cc
@@ -1,48 +1,48 @@
-#include <iostream>
 #include "DroneDamageDecorator.h"
-#include "WeatherControl.h"
-#include "SimulationModel.h"
-DroneDamageDecorator::DroneDamageDecorator(Drone* d)
-    : DroneDecorator(d) {
 
-    base_speed = d->getSpeed();
+#include <iostream>
+
+#include "SimulationModel.h"
+#include "WeatherControl.h"
+DroneDamageDecorator::DroneDamageDecorator(Drone* d) : DroneDecorator(d) {
+  base_speed = d->getSpeed();
 }
 // called every tick of the simulation
 void DroneDamageDecorator::update(double dt) {
-  //hover in place, maybe indicative that we let it land on the ground.
+  // hover in place, maybe indicative that we let it land on the ground.
   if (durability <= 0) return;
   sub->update(dt);
   Vector3 wind = WeatherControl::getInstance()->getWind();
-  //std::cout << wind << std::endl;
-  //scale by dt and a minimizing factor.
+  // std::cout << wind << std::endl;
+  // scale by dt and a minimizing factor.
   Vector3 windDisplacement = wind * dt * 0.1;
 
-  //handled movement here for OCP 
+  // handled movement here for OCP
   sub->setPosition(sub->getPosition() + windDisplacement);
 
-
-  //accumulator for damage logic
+  // accumulator for damage logic
   accum += dt;
   if (accum < 1) return;
   accum -= 1;
 
   double mag = wind.magnitude();
 
-  notifyObservers(std::to_string(durability) + "\% durability"); 
-  
+  notifyObservers(std::to_string(durability) + "\% durability");
+
   if (mag > 20) {
     durability -= mag / 50.0;
     double n_speed = base_speed * (durability / 100);
-    //capped speed loss, when it allowed it to go to 0, it was hard to watch lol 
-    if (n_speed < base_speed * 0.5) n_speed = base_speed* 0.5;
+    // capped speed loss, when it allowed it to go to 0, it was hard to watch
+    // lol
+    if (n_speed < base_speed * 0.5) n_speed = base_speed * 0.5;
     sub->setSpeed(n_speed);
-  } 
+  }
 
-  if (durability <= 0){
-    //drone broke :(
-    //throw package back in queue and let it handle itself. 
+  if (durability <= 0) {
+    // drone broke :(
+    // throw package back in queue and let it handle itself.
     sub->notifyObservers("Drone broke");
-    if(sub->getModel()){
+    if (sub->getModel()) {
       sub->getPackage()->unclaim();
     }
   }
