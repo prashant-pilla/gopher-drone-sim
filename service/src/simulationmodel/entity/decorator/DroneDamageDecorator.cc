3713ab0a7274b5dac438d7a9b3640cce1b45c972
Casey Paulson <paul1401@umn.edu>
2025-05-11 10:32:55 -0500

    changed damage rate for demo

--- service/src/simulationmodel/entity/decorator/DroneDamageDecorator.cc
+++ service/src/simulationmodel/entity/decorator/DroneDamageDecorator.cc
@@ -29,8 +29,11 @@ void DroneDamageDecorator::update(double dt) {
 
   notifyObservers(std::to_string(durability) + "\% durability");
   return;
-  if (mag > 20) {
-    durability -= mag / 50.0;
+  if (mag > 40) {
+    //take static 0.25% damage per second in sufficiently high winds
+    //value set low, and does not scale with mag for the sake of making 
+    //sure that drones lose battery faster than they take damage
+    durability -= 0.25;
     double n_speed = base_speed * (durability / 100);
     // capped speed loss, when it allowed it to go to 0, it was hard to watch
     // lol
